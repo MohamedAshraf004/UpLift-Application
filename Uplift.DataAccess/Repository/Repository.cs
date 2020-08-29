b39940ba682631b8a7b4801a3fcc89aaed8f1d16
Mohamed Ashraf <lionelmido004@gmail.com>
2020-08-29 17:20:00 +0200

    Init Project.

---
+++ Uplift.DataAccess/Repository/Repository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Uplift.DataAccess.IRepository;
+
+namespace Uplift.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _dbContext;
+
+        internal DbSet<T> dbSet;
+
+        public Repository(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter !=null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties !=null)
+            {
+                foreach (var propert in includeProperties.Split(new char[] { ','},StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(propert);
+                }
+            }
+            if (orderBy !=null)
+            {
+               return orderBy(query);
+            }
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
